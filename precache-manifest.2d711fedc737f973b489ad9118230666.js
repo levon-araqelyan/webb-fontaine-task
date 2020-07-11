7403e08d27623f04fa1493cc0a963ed5ca6eb631
levon-araqelyan <Levon.araqelyan.89@gmail.com>
2020-07-12 03:16:11 +0400

    Updates

---
+++ precache-manifest.2d711fedc737f973b489ad9118230666.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0634b377ef120794523d39794711836f",
+    "url": "/webb-fontaine-task/index.html"
+  },
+  {
+    "revision": "ed3781ba855a6631f05f",
+    "url": "/webb-fontaine-task/static/css/main.e56e3fe4.chunk.css"
+  },
+  {
+    "revision": "128657432aad17ef4c88",
+    "url": "/webb-fontaine-task/static/js/2.08e6c5ce.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/webb-fontaine-task/static/js/2.08e6c5ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed3781ba855a6631f05f",
+    "url": "/webb-fontaine-task/static/js/main.8699134f.chunk.js"
+  },
+  {
+    "revision": "d9a3dc1661aba542f179",
+    "url": "/webb-fontaine-task/static/js/runtime-main.85589926.js"
+  },
+  {
+    "revision": "a278e8685bd44b0d625d45f7c639a81e",
+    "url": "/webb-fontaine-task/static/media/Close.a278e868.svg"
+  },
+  {
+    "revision": "0331c51d5f93a996757d67ef76d54e77",
+    "url": "/webb-fontaine-task/static/media/delete.0331c51d.svg"
+  },
+  {
+    "revision": "0f623db5d2bfc3425e43d32b3dcb3d10",
+    "url": "/webb-fontaine-task/static/media/edit.0f623db5.svg"
+  },
+  {
+    "revision": "c910bf76ed2ee76f728367e5c9ea8969",
+    "url": "/webb-fontaine-task/static/media/logo.c910bf76.svg"
+  },
+  {
+    "revision": "23367ae0d3f91347b90210dca9735ffa",
+    "url": "/webb-fontaine-task/static/media/ok.23367ae0.svg"
+  },
+  {
+    "revision": "4d498ea4a396fc1946da78140f8d7b9f",
+    "url": "/webb-fontaine-task/static/media/settings.4d498ea4.svg"
+  }
+]);
